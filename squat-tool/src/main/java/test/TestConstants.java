a7c190f6ffedc4ec7f4c719b3bed4c8b44007acf
pfaehlfd <ferdinand.pfaehler@gmail.com>
2017-06-06 18:44:22 +0200

    removed my paths from TestConstants. The variables do not have to be final to enable the DockerConstants to overwrite the paths.

--- squat-tool/src/main/java/test/TestConstants.java
+++ squat-tool/src/main/java/test/TestConstants.java
@@ -3,22 +3,22 @@ package test;
 public class TestConstants {
 
 	// REMOVE FOR EXTERNAL START
-	// TODO: set paths 
-	
-	public static String DESIGNDECISION_FILE_PATH = "file:/D:/Mega/Uni/Master/Prozessanalyse/Workspace Repo/squat-tool/pcm/simpleexample.designdecision";
-	public static String QML_FILE_PATH = "D:/Mega/Uni/Master/Prozessanalyse/Workspace Repo/squat-tool/pcm/default.profile.qmldeclarations";
-	public static String QML_FILE_PATH2 = "D:/Mega/Uni/Master/Prozessanalyse/Workspace Repo/squat-tool/pcm/default2.profile.qmldeclarations";
+	// TODO: set paths
 
-	public static String USAGE_FILE_PATH = "D:\\Mega\\Uni\\Master\\Prozessanalyse\\Workspace Repo\\squat-tool\\pcm\\default.usagemodel";
-	public static String REPOSITORY_FILE_PATH = "D:\\Mega\\Uni\\Master\\Prozessanalyse\\Workspace Repo\\squat-tool\\pcm\\default.repository";
-	public static String RESOURCE_ENVIRONMENT_FILE_PATH = "D:\\Mega\\Uni\\Master\\Prozessanalyse\\Workspace Repo\\squat-tool\\pcm\\default.resourceenvironment";
-	public static String SYSTEM_FILE_PATH = "D:\\Mega\\Uni\\Master\\Prozessanalyse\\Workspace Repo\\squat-tool\\pcm\\default.system";
-	public static String ALLOCATION_FILE_PATH = "D:\\Mega\\Uni\\Master\\Prozessanalyse\\Workspace Repo\\squat-tool\\pcm\\default.allocation";
+	public static String DESIGNDECISION_FILE_PATH = "file:/C:/xxx/pcm/simpleexample.designdecision";
+	public static String QML_FILE_PATH = "C:/xxx/pcm/default.profile.qmldeclarations";
+	public static String QML_FILE_PATH2 = "C:/xxx/pcm/default2.profile.qmldeclarations";
 
-	public static String LQN_OUTPUT = "D:/Mega/Uni/Master/Prozessanalyse/Workspace Repo/squat-tool/Bot_out/pcmtests/lqnout";
-	public static String LQN_OUTPUT2 = "D:/Mega/Uni/Master/Prozessanalyse/Workspace Repo/squat-tool/Bot_out/pcmtests/lqnout2";
-	public static String PCM_STORAGE_PATH = "D:/Mega/Uni/Master/Prozessanalyse/Workspace Repo/squat-tool/Bot_out/pcmtests/testspace";
-	public static String PCM_STORAGE_PATH2 = "D:/Mega/Uni/Master/Prozessanalyse/Workspace Repo/squat-tool/Bot_out/pcmtests/testspace2";
-	public static String PCM_MODEL_FILES = "file:\\D:\\Mega\\Uni\\Master\\Prozessanalyse\\Workspace Repo\\squat-tool\\pcm\\defaultModels\\";
+	public static String USAGE_FILE_PATH = "C:\\xxx\\pcm\\default.usagemodel";
+	public static String REPOSITORY_FILE_PATH = "C:\\xxx\\pcm\\default.repository";
+	public static String RESOURCE_ENVIRONMENT_FILE_PATH = "C:\\xxx\\pcm\\default.resourceenvironment";
+	public static String SYSTEM_FILE_PATH = "C:\\xxx\\pcm\\default.system";
+	public static String ALLOCATION_FILE_PATH = "C:\\xxx\\pcm\\default.allocation";
+
+	public static String LQN_OUTPUT = "E:/xxx/pcmtests/lqnout";
+	public static String LQN_OUTPUT2 = "E:/xxx/pcmtests/lqnout2";
+	public static String PCM_STORAGE_PATH = "E:/xxx/pcmtests/testspace";
+	public static String PCM_STORAGE_PATH2 = "E:/xxx/pcmtests/testspace2";
+	public static String PCM_MODEL_FILES = "file:\\C:\\xxx\\PCM\\Core\\trunk\\PCM\\org.palladiosimulator.pcm.resources\\defaultModels\\";
 
 }
