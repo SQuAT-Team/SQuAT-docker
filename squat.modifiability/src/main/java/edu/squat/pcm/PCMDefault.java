a07515745a41035970c7d714760bff3899bd3e71
Sebastian Frank <s.frankausowen@googlemail.com>
2017-10-06 19:21:35 +0200

    Add building with docker for modifiability bot

--- squat.modifiability/src/main/java/edu/squat/pcm/PCMDefault.java
+++ squat.modifiability/src/main/java/edu/squat/pcm/PCMDefault.java
@@ -38,10 +38,10 @@ public class PCMDefault {
 	private static ResourceRepository repositoryResource = null;
 	private static Repository repositoryPrimitiveTypes = null;
 	private static Repository repositoryFailureTypes = null;
-	
+
 	static {
 		Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("*", new XMIResourceFactoryImpl());
-		//PCM Packages
+		// PCM Packages
 		EPackage.Registry.INSTANCE.put(IdentifierPackage.eNS_URI, IdentifierPackage.eINSTANCE);
 		EPackage.Registry.INSTANCE.put(StoexPackage.eNS_URI, StoexPackage.eINSTANCE);
 		EPackage.Registry.INSTANCE.put(CorePackage.eNS_URI, CorePackage.eINSTANCE);
@@ -54,75 +54,86 @@ public class PCMDefault {
 		EPackage.Registry.INSTANCE.put(SeffPackage.eNS_URI, SeffPackage.eINSTANCE);
 		EPackage.Registry.INSTANCE.put(ResourcetypePackage.eNS_URI, ResourcetypePackage.eINSTANCE);
 	}
-	
+
 	static {
 		Path currentPath = Paths.get("").toAbsolutePath();
 		Path workspacePath = currentPath.getParent();
 		String uriPath = workspacePath.normalize().toString();
-		//URI platformURI = URI.createPlatformPluginURI("org.palladiosimulator.pcm.resources", true).appendSegment("defaultModels").appendSegment("");  
-		//URI physicalURI = URI.createFileURI(uriPath).appendSegment("PCM").appendSegment("org.palladiosimulator.pcm.resources").appendSegment("defaultModels").appendSegment("");
-		URI physicalURI = URI.createFileURI(uriPath).appendSegment("squat.modifiability").appendSegment("pcm").appendSegment("defaultModels").appendSegment("");
+		// URI platformURI =
+		// URI.createPlatformPluginURI("org.palladiosimulator.pcm.resources",
+		// true).appendSegment("defaultModels").appendSegment("");
+		// URI physicalURI =
+		// URI.createFileURI(uriPath).appendSegment("PCM").appendSegment("org.palladiosimulator.pcm.resources").appendSegment("defaultModels").appendSegment("");
+
+		// URI physicalURI =
+		// URI.createFileURI(uriPath).appendSegment("squat.modifiability").appendSegment("pcm").appendSegment("defaultModels").appendSegment("");
+		// for docker:
+		URI physicalURI = URI.createFileURI("").appendSegment("home").appendSegment("pcm")
+				.appendSegment("defaultModels").appendSegment("");
 		URI pcmModels = URI.createURI("pathmap://PCM_MODELS/");
 		URIConverter.URI_MAP.put(pcmModels, physicalURI);
-		//URIConverter.URI_MAP.put(URI.createURI("pathmap://PCM_MODELS/"),URI.createURI("platform:/plugin/org.palladiosimulator.pcm.resources/defaultModels/"));
-		//URIConverter.URI_MAP.put(URI.createURI("pathmap://PCM_TRANSFORMATIONS/EVENTS/"),URI.createURI("platform:/plugin/org.palladiosimulator.pcm.resources/transformations/events/"));
+		// URIConverter.URI_MAP.put(URI.createURI("pathmap://PCM_MODELS/"),URI.createURI("platform:/plugin/org.palladiosimulator.pcm.resources/defaultModels/"));
+		// URIConverter.URI_MAP.put(URI.createURI("pathmap://PCM_TRANSFORMATIONS/EVENTS/"),URI.createURI("platform:/plugin/org.palladiosimulator.pcm.resources/transformations/events/"));
 	}
-	
+
 	public static ResourceRepository loadResourceRepository() {
-		if(repositoryResource == null) {
-			Resource resource = resourceSet.getResource(URI.createURI("pathmap://PCM_MODELS/Palladio.resourcetype"), true);
+		if (repositoryResource == null) {
+			Resource resource = resourceSet.getResource(URI.createURI("pathmap://PCM_MODELS/Palladio.resourcetype"),
+					true);
 			repositoryResource = (ResourceRepository) resource.getContents().get(0);
 		}
 		return repositoryResource;
 	}
-	
+
 	public static EList<ResourceInterface> loadResourceInterfaces() {
 		return PCMDefault.loadResourceRepository().getResourceInterfaces__ResourceRepository();
 	}
-	
+
 	public static EList<SchedulingPolicy> loadSchedulingPolicies() {
 		return PCMDefault.loadResourceRepository().getSchedulingPolicies__ResourceRepository();
 	}
-	
+
 	public static EList<ResourceType> loadAvailableResourceTypes() {
 		return PCMDefault.loadResourceRepository().getAvailableResourceTypes_ResourceRepository();
 	}
-	
+
 	public static EList<DataType> loadPrimitiveTypes() {
-		if(repositoryPrimitiveTypes == null) {
-			Resource resource = resourceSet.getResource(URI.createURI("pathmap://PCM_MODELS/PrimitiveTypes.repository"), true);
+		if (repositoryPrimitiveTypes == null) {
+			Resource resource = resourceSet.getResource(URI.createURI("pathmap://PCM_MODELS/PrimitiveTypes.repository"),
+					true);
 			repositoryPrimitiveTypes = (Repository) resource.getContents().get(0);
 		}
 		return repositoryPrimitiveTypes.getDataTypes__Repository();
 	}
-	
+
 	public static EList<FailureType> loadFailureTypes() {
-		if(repositoryPrimitiveTypes == null) {
-			Resource resource = resourceSet.getResource(URI.createURI("pathmap://PCM_MODELS/FailureTypes.repository"), true);
+		if (repositoryPrimitiveTypes == null) {
+			Resource resource = resourceSet.getResource(URI.createURI("pathmap://PCM_MODELS/FailureTypes.repository"),
+					true);
 			repositoryFailureTypes = (Repository) resource.getContents().get(0);
 		}
 		return repositoryFailureTypes.getFailureTypes__Repository();
 	}
-	
+
 	public static ProcessingResourceType getCPU() {
 		return (ProcessingResourceType) PCMDefault.loadAvailableResourceTypes().get(0);
 	}
-	
+
 	public static CommunicationLinkResourceType getLAN() {
 		return (CommunicationLinkResourceType) PCMDefault.loadAvailableResourceTypes().get(1);
 	}
-	
+
 	public static ProcessingResourceType getHDD() {
 		return (ProcessingResourceType) PCMDefault.loadAvailableResourceTypes().get(2);
 	}
-	
+
 	public static ProcessingResourceType getDELAY() {
 		return (ProcessingResourceType) PCMDefault.loadAvailableResourceTypes().get(3);
 	}
-	
+
 	public static void main(String[] args) {
 		EList<ResourceType> resources = PCMDefault.loadAvailableResourceTypes();
-		for(ResourceType resourceType : resources)
+		for (ResourceType resourceType : resources)
 			System.out.println(resourceType.getEntityName());
 	}
 }
---
+++ squat.modifiability/src/main/java/test/TestConstants.java
@@ -0,0 +1,8 @@
+package test;
+
+public class TestConstants {
+	public static final String MODEL_NAME = "default"; // cocome-cloud
+	public static final String MODEL_PATH = "/home/model"; // models/cocomeWithoutPickUpStoreAndServiceAdapter
+	public static final String ALTERNATIVE_REPOSITORY_PATH = "/home/model/alternativeRepository.repository"; // /Users/santiagovidal/Documents/Programacion/kamp-test/squat-tool/models/cocomeWithoutPickUpStoreAndServiceAdapter/alternativescocome-cloud.repository
+
+}
