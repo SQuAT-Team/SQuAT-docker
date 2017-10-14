a613c30433c9a5fafb434a160c07bea43264f369
roehrdor <roehrdor@outlook.com>
2017-10-14 18:49:56 +0200

    Context now stores rest architecture as well

--- squat.modifiability/src/main/java/test/ExecutionContext.java
+++ squat.modifiability/src/main/java/test/ExecutionContext.java
@@ -3,6 +3,7 @@ package test;
 import java.util.Objects;
 
 import io.github.squat_team.model.PCMArchitectureInstance;
+import io.github.squat_team.model.RestArchitecture;
 import io.github.squat_team.modifiability.kamp.KAMPPCMBot;
 
 public class ExecutionContext {
@@ -13,15 +14,21 @@ public class ExecutionContext {
     /** The architecture to analyze */
     private final PCMArchitectureInstance architectureInstance;
 
+    /** */
+    private final RestArchitecture restArchitecture;
+
     /** 
      *
      * @param bot
      * @param architectureInstance
+     * @param restArchitecture
      */
     public ExecutionContext(KAMPPCMBot bot, 
-            PCMArchitectureInstance architectureInstance) {
+            PCMArchitectureInstance architectureInstance,
+            RestArchitecture restArchitecture) {
         this.bot = Objects.requireNonNull(bot);
         this.architectureInstance = Objects.requireNonNull(architectureInstance);
+        this.restArchitecture = restArchitecture;
     }
 
     /**
@@ -36,5 +43,12 @@ public class ExecutionContext {
      */
     public PCMArchitectureInstance getArchitectureInstance() {
         return this.architectureInstance;
+    }
+
+    /**
+     * @return the rest architecture
+     */
+    public RestArchitecture getRestArchitecture() {
+        return this.restArchitecture;
     }
 }
